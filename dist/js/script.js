6f9b50af8e2e3d82891ebb7ddee3d495cbbd515e
Alexandr <alexandrgurov1@gmail.com>
2023-10-13 16:26:06 +0200

    start creating animations

--- dist/js/script.js
+++ dist/js/script.js
@@ -23,3 +23,37 @@ const counters = document.querySelectorAll(".skills__ratings-counter"),
 counters.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
 });
+
+$(document).ready(function () {
+  //Smooth scroll and pageup
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
+
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      const hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});

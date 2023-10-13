c512e01a96c9684cbf7b50b2d23c0bd9d8752622
Alexandr <alexandrgurov1@gmail.com>
2023-10-13 16:49:22 +0200

    animations created + copied parallax js file for future.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close");
+  closeElem = document.querySelector(".menu__close"),
+  links = document.querySelectorAll(".menu a");
 
 hamburger.addEventListener("click", () => {
   menu.classList.add("active");
@@ -8,6 +9,12 @@ hamburger.addEventListener("click", () => {
 
 closeElem.addEventListener("click", () => {
   menu.classList.remove("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
 });
 
 document.addEventListener("keydown", (e) => {
